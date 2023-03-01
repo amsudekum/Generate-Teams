09765734b383d98e6fdac49e9051a22c0a656c12
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-03-01 12:32:14 -0600

    added all requirements to index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const inquirer = require ('inquirer');
+//node
+
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require ('./lib/intern');
+//employee pages
+
+const createHTML = require ('./src/creatHTML');
+// the created page 
+
+
